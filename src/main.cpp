841fa9a3c0a8a1a5294c21bd7b7deb55534f6897
William Méroz-Moreau <merozwilliam@outloook.com>
2026-07-30 10:02:38 -0400

    wrote basic REPL, will add more functionnalities later.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
 
-int main() {
+class InputBuffer {
+public:
+    std::string buffer;
 
+    InputBuffer() = default;
+};
+
+void print_prompt() {
+    std::cout << "db >";
+}
+
+void read_input(InputBuffer &input) {
+    if (!std::getline(std::cin, input.buffer)) {
+        std::cerr << "Error reading input\n";
+        std::exit(EXIT_FAILURE);
+    }
+}
+
+int main() {
+    InputBuffer input;
+    while (true) {
+        print_prompt();
+        read_input(input);
+
+        if (input.buffer == ".exit") {
+            std::exit(EXIT_SUCCESS);
+        }
+
+        std::cout << "Unrecognized command '" << input.buffer << ".\n";
+    }
 
     return 0;
 }
